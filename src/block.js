eefe27d65f5cc58474e8c2504e875f37510ec4ca
skxrx <beliarsketcher@gmail.com>
2022-03-12 02:53:58 +0300

    Ext block class

--- src/block.js
+++ src/block.js
@@ -6,17 +6,30 @@ export default class Block {
     this.transactions = transactions
     this.previousHash = previousHash
     this.nonce = 0
-    this.hash = ''
+    this.hash = this.calculateHash()
   }
 
   calculateHash() {
     return SHA256(
-      this.timestamp + JSON.stringify(this.transactions) + this.nonce
-    )
+      this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.transactions) +
+        this.nonce
+    ).toString()
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
+    ) {
+      this.nonce++
+      this.hash = this.calculateHash()
+    }
+    console.log('Block mined, nonce: ' + this.nonce + ', hash: ' + this.hash)
   }
 
   hasValidTransactions() {
-    for (let tx of this.transactions) {
+    for (const tx of this.transactions) {
       if (!tx.isValid()) {
         return false
       }
