1fd6b4e04ad632710b1f1578b850f3fe9e05f61a
skxrx <beliarsketcher@gmail.com>
2022-03-12 02:54:10 +0300

    Ext blockchain class

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,5 @@
-const Block = require('./block')
+import Block from './block'
+import Transaction from './transaction'
 
 export default class Blockchain {
   constructor() {
@@ -14,5 +15,77 @@ export default class Blockchain {
 
   getLatestBlock() {
     return this.chain[this.chain.length - 1]
+  }
+
+  getHeight() {
+    return this.chain.length - 1
+  }
+
+  getBlock(height) {
+    return this.chain[height]
+  }
+
+  addTransaction(transaction) {
+    if (!transaction.fromAddress || !transaction.toAddress) {
+      throw new Error('transaction must include _from and _to addresses')
+    }
+
+    if (!transaction.isValid()) {
+      throw new Error('cant add invalid transaction')
+    }
+
+    this.pendingTransactions.push(transaction)
+  }
+
+  minePendingTransactions(miningRewardAddress) {
+    const latesBlock = this.getBlock(this.getHeight())
+
+    let block = new Block(Date.now(), this.pendingTransactions, latesBlock.hash)
+
+    block.mineBlock(this.difficulty)
+
+    console.log('### block mined successfully')
+    this.chain.push(block)
+
+    this.pendingTransactions = [
+      new Transaction(null, miningRewardAddress, this.miningReward),
+    ]
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0
+    for (let block of this.chain) {
+      for (let transaction of block.transactions) {
+        if (transaction.fromAddress === address) {
+          balance -= transaction.amount
+        }
+        if (transaction.toAddress === address) {
+          balance += transaction.amount
+        }
+      }
+    }
+    return balance
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i]
+      const previousBlock = this.chain[i - 1]
+
+      if (!currentBlock.hasValidTransactions()) {
+        return false
+      }
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        console.log(`hash not equal: ${JSON.stringify(currentBlock)}`)
+        return false
+      }
+
+      if (currentBlock.previousHash !== previousBlock.calculateHash()) {
+        console.log(`previous hash isnt right: ${JSON.stringify(currentBlock)}`)
+        return false
+      }
+    }
+    return true
   }
 }
